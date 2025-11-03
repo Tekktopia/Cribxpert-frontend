e83bdd2ed4faae83f2a5e227dcc4dd7d437b281c
samdahboss <samuelolawumi65@gmail.com>
2025-11-03 15:18:31 +0100

    Deleted SRC folder

--- src/utils/mockData.ts
+++
@@ -1,9 +0,0 @@
-// Export all mock data for easy importing
-export * from './imageConstants';
-export * from './bedroomData';
-export * from './locationData';
-export * from './houseRulesData';
-export * from './cancellationData';
-export * from './ratingData';
-export * from './reviewsData';
-export * from './descriptionData';
